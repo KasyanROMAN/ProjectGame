d3305bd1fa96d334b913d7ab17eb9eb0c4dcb94a
Maxim Andropov <maxikandropov@gmail.com>
2025-04-05 20:21:48 +0800

    admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Game
+
+@admin.register(Game)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','link','description']
+    search_fields = ['name']
--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from product.models import Game
+
 # Create your views here.
+
+def index(request):
+    games = Game.objects.all()
+    return render(request,'index.html',{'games':games})
